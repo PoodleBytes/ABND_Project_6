5ee13ae3d3eda30f384a73e2682795c38de8a45f
PoodleBytes <jr@robitaille.us>
2018-09-01 18:35:01 -0400

    Initial Commit 9/1/2018

---
+++ app/src/main/java/com/udacity/poodlebytes/abnd_project_6/MainActivity.java
@@ -0,0 +1,137 @@
+package com.udacity.poodlebytes.abnd_project_6;
+
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<News>> {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String API_KEY = BuildConfig.ApiKey;
+
+    /**
+     * URL for earthquake data from the Guardian dataset
+     */
+    private static final String REQUEST_URL =
+            "http://content.guardianapis.com/search?from-date=2018-08-01&to-date=2018-09-01&&api-key=" +
+                    API_KEY +
+                    "&show-tags=contributor&page-size=20";
+
+    /**
+     * Constant for newsLoader ID
+     */
+    private static final int NEWS_LOADER_ID = 1;
+
+    /**
+     * Adapter for the list of earthquakes
+     */
+    private NewsAdapter adapter;
+
+    /**
+     * TextView that is displayed when the list is empty
+     */
+    private TextView emptyStateTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_activity);
+
+        ListView newsListView = findViewById(R.id.list);
+
+        emptyStateTextView = findViewById(R.id.empty_view);
+        newsListView.setEmptyView(emptyStateTextView);
+
+        // Create a new adapter
+        adapter = new NewsAdapter(this, new ArrayList<News>());
+
+        // assign listView to adapter
+        newsListView.setAdapter(adapter);
+
+        // Intent to open a website w/selected article
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current article
+                News currentNews = adapter.getItem(position);
+
+                // Convert the String URL into a URI object
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+
+                // Create a new intent to view the URL
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+                // launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+        // check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            emptyStateTextView.setText(R.string.no_internet_connection);
+        }
+    }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        // Create a new loader for the given URL
+        return new NewsLoader(this, REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No news found."
+        emptyStateTextView.setText(R.string.no_news);
+
+        // If there is a valid News add them to the adapter's
+        if (news != null && !news.isEmpty()) {
+            adapter.addAll(news);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        adapter.clear();
+    }
+}
---
+++ app/src/main/java/com/udacity/poodlebytes/abnd_project_6/QueryUtils.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.udacity.poodlebytes.abnd_project_6;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Helper methods related to requesting and receiving earthquake data from USGS.
+ */
+public final class QueryUtils {
+
+    /**
+     * Tag for the log messages
+     */
+    private static final String TAG = QueryUtils.class.getSimpleName();
+
+    /**
+     * Empty Constructor
+     */
+    private QueryUtils() {
+    }
+
+    /**
+     * Query the Guardian API and return a list of {@link News} objects.
+     */
+    public static List<News> fetchNewsData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(TAG, "Problem making the HTTP request.", e);
+        }
+
+        // Extract relevant fields from the JSON response and create a News list
+        List<News> news = extractFeatureFromJson(jsonResponse);
+
+        // Return the list
+        return news;
+    }
+
+    /**
+     * Returns new URL object from the given string URL.
+     */
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    /**
+     * Make an HTTP request to the given URL and return a String as the response.
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException ie) {
+            Log.e(TAG, "Problem retrieving JSON results.", ie);
+        } finally {     //close connection & inputStream
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into String from the JSON response
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    /**
+     * Return a News array from JSON response.
+     */
+    private static List<News> extractFeatureFromJson(String newsJSON) {
+        // If the JSON string is empty or null, then return early.
+        if (TextUtils.isEmpty(newsJSON)) {
+            Log.i(TAG, "NULL" + newsJSON);
+            return null;
+        }
+
+        // Create an empty ArrayList & start adding articles
+        List<News> news = new ArrayList<>();
+
+        // Try to parse the JSON response string.
+        try {
+            Log.i(TAG, "Start Parsing " + newsJSON);
+            // Create a JSONObject from the JSON response string
+            JSONObject baseJsonResponse = new JSONObject(newsJSON);
+            JSONObject baseJsonResult = baseJsonResponse.getJSONObject("response");
+            JSONArray newsArray = baseJsonResult.getJSONArray("results");
+
+            // For each news article create a news  object
+            for (int i = 0; i < newsArray.length(); i++) {
+
+                // Get a single article at position i within the JSON array
+                JSONObject currentNews = newsArray.getJSONObject(i);
+
+
+                // Parse JSON values:
+                //article title
+                String title = currentNews.getString("webTitle");
+
+                //date
+                String rawdate = currentNews.getString("webPublicationDate");
+                String[] dateAndTime;
+                dateAndTime = rawdate.split("T");
+                String date = dateAndTime[0];
+
+                //article "url"
+                String url = currentNews.getString("webUrl");
+
+                //get author from tags portion of JSON Object
+                JSONArray authorArray = currentNews.getJSONArray("tags");
+                JSONObject currentAuthor = authorArray.getJSONObject(0);
+                String authorName = currentAuthor.getString("webTitle");
+                //Concatenation of author name and type of author (pulled from JSON)
+                StringBuilder authorBuilder = new StringBuilder();
+                authorBuilder.append(authorName);
+                //Check for 2nd author
+                if (authorArray.length() > 1) {
+                    JSONObject secondaryAuthor = authorArray.getJSONObject(1);
+                    String secondAuthor = secondaryAuthor.getString("webTitle");
+                    authorBuilder.append(" & ");
+                    authorBuilder.append(secondAuthor);
+                }
+                String author = authorBuilder.toString();
+
+                //article category
+                String category = currentNews.getString("sectionName");
+
+                // Create a news object from the JSON response.
+                News myNews = new News(title, date, url, author, category);
+
+                // Add the article to the list
+                news.add(myNews);
+            }
+        } catch (JSONException e) {
+            Log.e("QueryUtils", "Problem parsing JSON results", e);
+        }
+
+        // Return the list of earthquakes
+        return news;
+    }
+}
